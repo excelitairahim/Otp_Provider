85df93890ec671fe5350996a75140e1db5fcce7c
excelitrahim <rahim@excelitai.com>
2022-08-30 09:34:31 +0600

    .

--- lib/view/about.dart
+++ lib/view/about.dart
@@ -3,6 +3,7 @@ import 'package:flutter/src/foundation/key.dart';
 import 'package:flutter/src/widgets/framework.dart';
 import 'package:otp_provider/view/drawer.dart';
 import 'package:otp_provider/theme/theme.dart';
+import 'package:url_launcher/url_launcher.dart';
 class AboutPage extends StatefulWidget {
   const AboutPage({Key? key}) : super(key: key);
 
@@ -13,10 +14,11 @@ class AboutPage extends StatefulWidget {
 class _AboutPageState extends State<AboutPage> {
   @override
   Widget build(BuildContext context) {
-    return Scaffold( 
-      
+    return Scaffold(
       appBar: AppBar(
-        title: Text('About',),
+        title: Text(
+          'About',
+        ),
         centerTitle: true,
         backgroundColor: theme.primaryColor.withOpacity(.8),
         leading: Builder(
@@ -28,6 +30,20 @@ class _AboutPageState extends State<AboutPage> {
                 icon: Icon(Icons.menu));
           },
         ),
-      ),body: Center(child: Text('About Page',style: TextStyle(fontSize: 20,fontWeight: FontWeight.bold,color:theme.titleTextColor),),),);
+      ),
+      body: Column(
+        children: [
+          Center(
+            child: Text(
+              'About Page',
+              style: TextStyle(
+                  fontSize: 20,
+                  fontWeight: FontWeight.bold,
+                  color: theme.titleTextColor),
+            ),
+          ),
+      ],
+      ),
+    );
   }
 }
